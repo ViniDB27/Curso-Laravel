6cac1513b2b15087880dda57f35648042af8c23d
Vinicio D. Brejinski <viniciosdb@gmail.com>
2020-08-18 22:53:10 -0300

    validator

--- todo/routes/api.php
+++ todo/routes/api.php
@@ -4,4 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get("tasks", "TasksController@index");
+Route::get("tasks/{task}", "TasksController@show");
+Route::patch("tasks/{task}", "TasksController@update");
+Route::delete("tasks/{task}", "TasksController@destroy");
 Route::post("tasks", "TasksController@store");
