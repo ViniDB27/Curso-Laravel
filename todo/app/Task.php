069f0db79776ec237eaa45f9eb393a248d1ba78c
Vinicio D. Brejinski <viniciosdb@gmail.com>
2020-08-18 21:55:31 -0300

    rota post e migrations

--- todo/app/Task.php
+++ todo/app/Task.php
@@ -4,7 +4,8 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Task extends Model
-{
-    protected $filelable = ['name', 'complete'];
+class Task extends Model {
+
+    protected $fillable = ['name', 'complete'];
+
 }
